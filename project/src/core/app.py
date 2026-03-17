2abf696f7705c86fff7332e11896523b4eff2548
Aniello Piscopo <aniello.piscopo0707@gmail.com>
2026-03-17 17:13:35 +0100

    Added the managment of a custom exception in app.py

--- project/src/core/app.py
+++ project/src/core/app.py
@@ -1,9 +1,10 @@
 from core.engine import play
+from exceptions import OutOfRangeError
 
 __all__ = ['start']
 
 def start()->None:
     try:
         play()
-    except ValueError as e:
+    except (ValueError,OutOfRangeError) as e:
         print(e)
